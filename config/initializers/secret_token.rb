71454512a436afe090248032cfcf4507bc462225
Andrew Popov <andrewpopov07@mail.ru>
2017-01-11 15:52:41 +1000

    Инициализация репозитория

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twitter::Application.config.secret_token = '664794472148e8ea14da0f7b93dc6bbef553d5d45f6f431535a8ae53ba9e6e99a7928adc205ef2e49ec49ae987b2e0574f1baa996dfb3ef12f44d0198ca7e622'
